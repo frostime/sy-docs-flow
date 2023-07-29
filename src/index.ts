11c0407b883c3a6d70b2877ef179f800b4fcccff
frostime <frostime@foxmail.com>
2023-07-29 22:14:41 +0800

    feat: 多个 Tab 管理

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import {
     openTab,
     getFrontend,
     getBackend,
+    IModel,
     Menu
 } from "siyuan";
 import "@/index.scss";
@@ -18,12 +19,87 @@ import { MatchRule, RuleFactory } from "@/rules";
 const frontEnd = getFrontend();
 const isMobile = frontEnd === "mobile" || frontEnd === "browser-mobile";
 
+
+class TabHub {
+    plugin: Plugin;
+    tabs: {
+        [key: string]: {
+            rule: MatchRule;
+            tab: () => IModel;
+        }
+    }
+
+    constructor(plugin: Plugin) {
+        this.plugin = plugin;
+        this.tabs = {};
+    }
+
+    async open(rule: MatchRule) {
+        let hash = rule.hash;
+        if (this.tabs[hash]) {
+            this.openTab(hash);
+            return;
+        }
+        let ids = await rule.getIds();
+        if (!ids || ids.length === 0) {
+            showMessage("无法匹配对应的文档");
+            return;
+        }
+        let tabDiv = document.createElement("div");
+        let flow = new DocsFlow({
+            target: tabDiv,
+            props: {
+                app: this.plugin.app,
+                listDocuemntsId: ids
+            }
+        });
+
+        const Tabs = this.tabs;
+        let tab = this.plugin.addTab({
+            type: 'custom_tab',
+            init() {
+                this.element.appendChild(tabDiv);
+            },
+            beforeDestroy() {
+                flow.$destroy();
+                tabDiv.remove();
+            },
+            destroy: () => {
+                delete Tabs[hash];
+                console.log("destroy tab:", hash);
+            }
+        });
+        this.tabs[hash] = {
+            rule,
+            tab
+        };
+        this.openTab(hash);
+    }
+
+    private openTab(hash: any) {
+        let tab = this.tabs[hash].tab;
+        let rule = this.tabs[hash].rule;
+        openTab({
+            app: this.plugin.app,
+            custom: {
+                icon: "iconFlow",
+                title: `流式文档-${rule.type}`,
+                data: hash, //关键, 思源靠这个判断是否是同一个tab
+                fn: tab,
+            },
+            keepCursor: false,
+            removeCurrentTab: true
+        });
+        console.log(this.tabs);
+    }
+}
+
 export default class DocsFlowPlugin extends Plugin {
 
-    tab: any;
+    tabHub: TabHub;
 
     async onload() {
-
+        this.tabHub = new TabHub(this);
         console.log("loading plugin-sample", this.i18n);
         // 图标的制作参见帮助文档
         this.addIcons(`<symbol id="iconFlow" viewBox="0 0 1024 1024"><path d="M1024 640v384H0v-384h128v256h768v-256zM192 704h640v128H192z m15.168-138.56l27.712-124.992 624.832 138.496L832 703.936zM279.68 308.544l54.08-116.032 580.032 270.464-54.08 116.032z m712.064 52.928l-77.952 101.568-507.776-389.632L462.336 0h58.24z" p-id="7558"></path></symbol>`);
@@ -67,7 +143,7 @@ export default class DocsFlowPlugin extends Plugin {
             icon: "iconInfo",
             label: "子文裆",
             click: () => {
-                this.openFlow(RuleFactory("ChildDocument"));
+                this.tabHub.open(RuleFactory("ChildDocument"));
             }
         });
         menu.addItem({
@@ -82,11 +158,11 @@ export default class DocsFlowPlugin extends Plugin {
                         showMessage("SQL语句不正确");
                         return;
                     }
-                    this.openFlow(RuleFactory("SQL", sql));
+                    // this.openFlow(RuleFactory("SQL", sql));
+                    this.tabHub.open(RuleFactory("SQL", sql));
                 });
             }
         });
-
 
         if (isMobile) {
             menu.fullscreen();
@@ -97,43 +173,6 @@ export default class DocsFlowPlugin extends Plugin {
                 isLeft: true,
             });
         }
-    }
-
-    async openFlow(rule: MatchRule) {
-        let ids = await rule.getIds();
-        if (!ids || ids.length === 0) {
-            showMessage("无法匹配对应的文档");
-            return;
-        }
-
-        let tabDiv = document.createElement("div");
-        new DocsFlow({
-            target: tabDiv,
-            props: {
-                app: this.app,
-                listDocuemntsId: ids
-            }
-        });
-        let tab = this.addTab({
-            type: 'custom_tab',
-            init() {
-                this.element.appendChild(tabDiv);
-                console.log(this.element);
-            },
-            beforeDestroy() {
-            },
-            destroy() {
-            }
-        });
-        // this.tab.open();
-        openTab({
-            app: this.app,
-            custom: {
-                icon: "iconFlow",
-                title: "流式文档",
-                fn: tab
-            },
-        })
     }
 
     /**
@@ -155,7 +194,3 @@ export default class DocsFlowPlugin extends Plugin {
         });
     }
 }
-
-const tabhub = {
-    tabs: [],
-};
