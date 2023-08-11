58b55bcc86982dc7e334976bbbaa26bbfeb7d07a
frostime <frostime@foxmail.com>
2023-08-11 15:24:59 +0800

    enhance: 优化了折叠功能, 通过折叠来动态加载 Protyle

--- src/components/docs-flow/protyle.svelte
+++ src/components/docs-flow/protyle.svelte
@@ -3,11 +3,11 @@
  Author       : Yp Z
  Date         : 2023-07-28 21:14:31
  FilePath     : /src/components/docs-flow/protyle.svelte
- LastEditTime : 2023-08-11 12:55:48
+ LastEditTime : 2023-08-11 15:20:07
  Description  : 
 -->
 <script lang="ts">
-    import { onDestroy, onMount } from "svelte";
+    import { onDestroy, onMount, afterUpdate } from "svelte";
     import { Protyle, openTab } from "siyuan";
     import { getBlockByID } from "../../api";
     import { notebooks } from "../../utils";
@@ -24,24 +24,23 @@
 
     let divGutter: HTMLDivElement;
 
-    let heightBreadcrumb: number;
+    let initialised: boolean = false;
+
+    let heightBreadcrumb: number = 40;
 
     let styleProtyleMaxHeight: string = "";
+    const updateProtyleMaxHeight = () => {
+        let maxHeight: number = scroll ? setting.getMaxHeight() : null;
+        if (maxHeight) {
+            maxHeight = maxHeight - 2 * heightBreadcrumb;
+        }
+        styleProtyleMaxHeight = maxHeight ? `max-height: ${maxHeight}px;` : "";
+    };
 
     let expanded: boolean = true;
     let classArrowOpen: string = "";
-    let styleDisplayProtyle: string = "";
     $: {
         classArrowOpen = expanded ? "b3-list-item__arrow--open" : "";
-        styleDisplayProtyle = expanded ? "" : "display: none";
-    }
-
-    $: {
-        let maxHeight: number = scroll ? setting.getMaxHeight() : null;
-        if (maxHeight) {
-            maxHeight = maxHeight - heightBreadcrumb * 3;
-        }
-        styleProtyleMaxHeight = maxHeight ? `max-height: ${maxHeight}px;` : "";
     }
 
     onMount(async () => {
@@ -51,15 +50,32 @@
         let notebookName: string = notebooks[doc.box];
         let prefix = notebookName ? `/${notebookName}` : "";
         hpath = prefix + doc.hpath;
-        console.log(notebookName, hpath);
-        load();
+        console.log('Mount protyle:', notebookName, hpath, blockId);
+        initialised = true;
     });
     onDestroy(() => {
         // protyle?.destroy();
         unload();
     });
 
+    afterUpdate(() => {
+        if (!initialised) {
+            return; //由于 onMunt 是 async 所以会出现还没有执行完毕就调用了 afterUpdate 的情况
+        }
+        console.log("afterUpdated", blockId, expanded);
+        if (divProtyle && expanded) {
+            load();
+        } else if (!divProtyle && !expanded) {
+            unload();
+        }
+    });
+
     function load() {
+        if (!divProtyle) {
+            return;
+        }
+        console.log("Load protyle...", blockId);
+        updateProtyleMaxHeight();
         protyle = new Protyle(app, divProtyle, {
             mode: "wysiwyg",
             blockId: blockId,
@@ -77,8 +93,8 @@
     }
 
     function unload() {
+        console.log('Unload protyle...', blockId);
         protyle?.destroy();
-        divProtyle.innerHTML = "";
         divGutter = null;
     }
 
@@ -94,7 +110,6 @@
         class="b3-list-item b3-list-item--hide-action protyle-breadcrumb__item"
         data-node-id={blockId}
         data-type="NodeDocument"
-        bind:clientHeight={heightBreadcrumb}
         on:keypress={() => {}}
         on:click={(e) => {
             openTab({
@@ -125,13 +140,15 @@
             ><use xlink:href="#iconFile" /></svg
         >
     </li>
-    <div
-        class="docs-flow__protyle"
-        bind:this={divProtyle}
-        style="{styleProtyleMaxHeight} {styleDisplayProtyle}"
-        on:mouseenter={() => toggleGutterDisplay(true)}
-        on:mouseleave={() => toggleGutterDisplay(false)}
-    />
+    {#if expanded}
+        <div
+            class="docs-flow__protyle"
+            bind:this={divProtyle}
+            style="{styleProtyleMaxHeight}"
+            on:mouseenter={() => toggleGutterDisplay(true)}
+            on:mouseleave={() => toggleGutterDisplay(false)}
+        />
+    {/if}
 </div>
 
 <style lang="scss">
